9ee8cd40748294179c9f3e316f9bd8c1450e3017
danielvera987 <danielveraangulo703@gmail.com>
2020-11-14 21:46:32 -0500

    add sql

--- database/db.sql
+++ database/db.sql
@@ -1,13 +1,18 @@
-CREATE DATABASE miniframework;
+CREATE DATABASE `miniframework`;
 
 USE miniframework;
 
-CREATE TABLE `miniframework`.`user` ( 
-  `id` INT(255) NOT NULL AUTO_INCREMENT , 
-  `nombre` VARCHAR(255) NOT NULL , 
-  `email` VARCHAR(255) NOT NULL , 
-  `password` VARCHAR(255) NOT NULL , 
-  `salt` VARCHAR(255) NOT NULL , 
-  `created_at` DATE NOT NULL , 
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `salt` varchar(255) NOT NULL,
+  `created_at` date NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE = InnoDB; 
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+INSERT INTO `user` (`id`, `nombre`, `email`, `password`, `salt`, `created_at`) VALUES
+(8, 'Daniel', 'test@test.com', '$2y$10$MwG0fxpF2X3F1TdOSyBDvOABlp8uA7ZflbKpsgByMiYXvbYXat7J.', 'D2N2CQaM4Ys=', '2020-11-15');
+COMMIT;
